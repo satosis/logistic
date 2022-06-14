79e57953679b48d1b089930c66dcb37031f32021
nguyenhungbm <hungbm@mor.com.vn>
2022-06-15 06:01:15 +0700

    update

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Admin; 
@@ -12,15 +13,15 @@ use App\Models\Transaction;
 class AdminController extends Controller
 {
     public function index(){
-      $totalTransaction = \DB::table('transactions')->select('id')->count();
+      $totalTransaction = DB::table('transactions')->select('id')->count();
       $totalUser        = User::select('id')->count();
-      $totalRating      = \DB::table('ratings')->select('id')->count();
+      $totalRating      = DB::table('ratings')->select('id')->count();
       $hotProduct       = Product::orderByDesc('pro_pay')->limit(5)->get();
       //thống kê trạng thái đơn hàng
-      $transactionDefault =\DB::table('transactions')->where('tst_status',1)->select('id')->count();
-      $transactionProcess =\DB::table('transactions')->where('tst_status',2)->select('id')->count();
-      $transactionSuccess =\DB::table('transactions')->where('tst_status',3)->select('id')->count();
-      $transactionCanCel =\DB::table('transactions')->where('tst_status',-1)->select('id')->count();
+      $transactionDefault = DB::table('transactions')->where('tst_status',1)->select('id')->count();
+      $transactionProcess = DB::table('transactions')->where('tst_status',2)->select('id')->count();
+      $transactionSuccess = DB::table('transactions')->where('tst_status',3)->select('id')->count();
+      $transactionCanCel = DB::table('transactions')->where('tst_status',-1)->select('id')->count();
       $statusTransaction =[
         ['Đã thanh toán', $transactionSuccess, false], 
         ['Đang chờ thanh toán', $transactionProcess, false], 
@@ -29,19 +30,20 @@ class AdminController extends Controller
         ];
       
       $listDay =Date::getListDayAndMonth(); 
-        //doanh thu theo tháng đã xử lý
-        $revenueTransactionMonthDefault = Transaction::where('tst_status', 3)
+      //doanh thu theo tháng đã xử lý
+      $revenueTransactionMonthDefault = Transaction::where('tst_status', 3)
         ->whereMonth('created_at',date('m'))
-        ->select(\DB::raw("sum(tst_total_money) as totalMoney"), \DB::raw('DATE(created_at) as day'))
+        ->select(DB::raw("sum(cast(tst_total_money as int)) as totalMoney"), DB::raw('DATE(created_at) as day'))
         ->groupBy('day')
-        ->get()->toArray(); 
-
-          //doanh thu theo tháng chưa xử lý
-          $revenueTransactionMonth = Transaction::where('tst_status', 1)
-          ->whereMonth('created_at',date('m'))
-          ->select(\DB::raw("sum(tst_total_money) as totalMoney"), \DB::raw('DATE(created_at) as day'))
-          ->groupBy('day')
-          ->get()->toArray();
+        ->get()
+        ->toArray(); 
+      //doanh thu theo tháng chưa xử lý
+      $revenueTransactionMonth = Transaction::where('tst_status', 1)
+        ->whereMonth('created_at',date('m'))
+        ->select(DB::raw("sum(cast(tst_total_money as int)) as totalMoney"), DB::raw('DATE(created_at) as day'))
+        ->groupBy('day')
+        ->get()
+        ->toArray();
     $arrRevenueTransactionMonth = [];
     $arrRevenueTransactionMonthDefault = [];
     foreach($listDay as $day) {
