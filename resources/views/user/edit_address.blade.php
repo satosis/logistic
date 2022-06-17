5d663be0f9604beca8be2786de6911e88e22705e
nguyenhungbm <hungbm@mor.com.vn>
2022-06-17 11:20:42 +0700

    update

--- resources/views/user/edit_address.blade.php
+++ resources/views/user/edit_address.blade.php
@@ -27,15 +27,12 @@
                            <div class="mod-input floating mod-input-detailAddress">
                               <label>Địa chỉ nhận hàng</label><input type="text" name="location" placeholder="Vui lòng nhập địa chỉ của bạn" value="{{ \Auth::user()->location }}"><b></b><span></span>
                               <div class="mod-input-close-icon"></div>
-                           </div>
-                           <!-- <div class="mod-select floating mod-address-form-select mod-select-location-tree-1"><label>Tỉnh/ Thành phố</label><span value="R1875748---An Giang" class="next-select large" tabindex="0"><input type="hidden" name="select-faker" value="R1875748---An Giang"><span class="next-select-inner">An Giang</span><i class="next-icon next-icon-arrow-down next-icon-medium next-select-arrow"></i></span><b></b></div>
-                           <div class="mod-select floating mod-address-form-select mod-select-location-tree-2"><label>Quận/ Huyện</label><span value="R7147138---Thành Phố Châu Đốc" class="next-select large" tabindex="0"><input type="hidden" name="select-faker" value="R7147138---Thành Phố Châu Đốc"><span class="next-select-inner">Thành Phố Châu Đốc</span><i class="next-icon next-icon-arrow-down next-icon-medium next-select-arrow"></i></span><b></b></div>
-                           <div class="mod-select floating mod-address-form-select mod-select-location-tree-3"><label>Phường/ Xã</label><span value="R80002981---Phường Châu Phú B" class="next-select large" tabindex="0"><input type="hidden" name="select-faker" value="R80002981---Phường Châu Phú B"><span class="next-select-inner">Phường Châu Phú B</span><i class="next-icon next-icon-arrow-down next-icon-medium next-select-arrow"></i></span> </div > -->
+                           </div> 
                         </div>
                      </div>
                      <div class="mod-address-form-action"><a href="{{ route('get.user.address')}}" class="next-btn next-btn-normal next-btn-medium mod-address-form-btn-cancel">HUỶ</a><button tabindex="7" type="submit" class="next-btn next-btn-primary next-btn-large mod-address-form-btn">LƯU</button></div>
                   </div>
-               </form>< 
+               </form>
             </div>
          </div>
       </div>
